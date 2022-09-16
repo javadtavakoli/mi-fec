ddca5acd835efcc68429093d471363542f675c6a
Javad Tavakoli <javadtavakoli95@gmail.com>
2022-09-16 19:09:36 +0430

    changed videos service to return videos

--- src/services/videos.ts
+++ src/services/videos.ts
@@ -3,9 +3,22 @@ import AuthorsServices from './authors';
 import CategoriesServices from './categories';
 
 const Get = async (): Promise<ProcessedVideo[]> => {
-  const [categories, authors] = await Promise.all([CategoriesServices.Get(), AuthorsServices.Get()]);
-
-  return []; // TODO: implement
+  const [categoriesResponse, authorsResponse] = await Promise.all([CategoriesServices.Get(), AuthorsServices.Get()]);
+  const authors = authorsResponse.data;
+  const categories = categoriesResponse.data;
+  const proccessedVideos: ProcessedVideo[] = [];
+  authors.forEach((author) => {
+    const authorVideos = author.videos.map<ProcessedVideo>((av) => ({
+      author: author.name,
+      categories: av.catIds.map((catId) => {
+        return categories.find((c) => c.id === catId)?.name || '';
+      }),
+      id: av.id,
+      name: av.name,
+    }));
+    proccessedVideos.push(...authorVideos);
+  });
+  return proccessedVideos;
 };
 const VideosServices = { Get };
 export default VideosServices;
