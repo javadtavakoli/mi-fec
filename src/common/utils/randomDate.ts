d9a66afa6ab3d79f672978eb80c080bdb3c69845
Javad Tavakoli <javadtavakoli95@gmail.com>
2022-09-17 17:18:06 +0430

    fixed random date

--- src/common/utils/randomDate.ts
+++ src/common/utils/randomDate.ts
@@ -4,7 +4,7 @@ const Generate = (): string => {
   const start = new Date(RANDOM_START_DATE);
   const end = new Date();
   const randomDate = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-  return `${randomDate.getFullYear()}-${randomDate.getMonth()}-${randomDate.getDate}`;
+  return `${randomDate.getFullYear()}-${randomDate.getMonth()}-${randomDate.getDate()}`;
 };
 const RandomDate = { Generate };
 export default RandomDate;
