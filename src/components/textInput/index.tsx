65d42a54fa9acf31270e75a7c46a103527923fa6
Javad Tavakoli <javadtavakoli95@gmail.com>
2022-09-17 14:09:48 +0430

    added text input

---
+++ src/components/textInput/index.tsx
@@ -0,0 +1,6 @@
+import { InputHTMLAttributes } from 'react';
+
+const TextInput = (props: InputHTMLAttributes<HTMLInputElement>) => {
+  return <input type="text" {...props} />;
+};
+export default TextInput;
