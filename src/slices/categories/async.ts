c56994daaf836680b7e667f2b6550126c3c65100
Javad Tavakoli <javadtavakoli95@gmail.com>
2022-09-17 12:19:11 +0430

    fixed categories action name

--- src/slices/categories/async.ts
+++ src/slices/categories/async.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { Category } from '../../common/interfaces';
 import CategoriesServices from '../../services/categories';
 
-const GetAll = createAsyncThunk<Category[], void>('categories', async (_) => {
+const GetAll = createAsyncThunk<Category[], void>('categories/getAll', async (_) => {
   const categoriesResponse = await CategoriesServices.Get();
   return categoriesResponse.data;
 });
