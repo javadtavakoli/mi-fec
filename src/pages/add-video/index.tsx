36dd731f8f503bdb2ea615680e4732435bcece50
Javad Tavakoli <javadtavakoli95@gmail.com>
2022-09-17 21:46:21 +0430

    fixed page heading style

--- src/pages/add-video/index.tsx
+++ src/pages/add-video/index.tsx
@@ -1,11 +1,12 @@
 import VideoForm from '../../components/videoForm';
 import useAddVideo from '../../hooks/addVideo';
+import headingStyle from '../../common/css/heading.module.css';
 
 const AddVideoPage = () => {
   const { submit, cancel } = useAddVideo();
   return (
     <>
-      <h1>Add Video</h1>
+      <h1 className={headingStyle.pageHeading}>Add Video</h1>
       <VideoForm cancel={cancel} submit={submit} initialValues={{ name: '', catIds: [], authorId: -1 }} />
     </>
   );
