d282cf1e722347e02d69a649c563829d687de2b8
Javad Tavakoli <javadtavakoli95@gmail.com>
2022-09-17 12:43:51 +0430

    removed name from authors select

--- src/components/authorsSelect/index.tsx
+++ src/components/authorsSelect/index.tsx
@@ -3,7 +3,7 @@ import useLoadAuthors from '../../hooks/loadAuthors';
 const AuthorsSelect = () => {
   const { authors } = useLoadAuthors();
   return (
-    <select id="author" name="author">
+    <select id="author">
       {authors.map((author) => (
         <option key={author.id.toString()} value={author.id}>
           {author.name}
